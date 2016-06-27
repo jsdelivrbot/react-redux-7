4ad7b6dc5b4e9dc053c9e2106957eba75b02b493
tillman-facecake <touchbase@richtillman.com>
2016-06-27 15:07:35 -0700

    New components
    Added 4 component files to create the video search application

---
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -0,0 +1,9 @@
+// NOTE:  import react into any and all project files that use JSX
+import React from 'react';
+
+const SearchBar = () => {
+	return <input />;
+};
+
+export default SearchBar;
+
--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
-// Create new component.  This component should produce some HTML
+// YouTube API key
+const API_KEY = 'AIzaSyCATEQ8h-V4beHQrgUo9Swcs_6q7oNUxT4';
+
+// Create new component.  This component should produce some HTML - ALWAYS ONE COMPONENT PER FILE!
 
 /* NOTE: "const" is essentially a variable or class that doesn't change.
 App will never equal 5 or anything other than the assigned function */
 // NOTE: => (fat arrow) is used in place of 'function' and serves an identical purpose.
+// NOTE: When using multi-line JSX the common practice is to wrap in parentheses
 const App = () => {
-	return <div>Hi!</div>; //this is JSX
-}
+	return (
+		<div>
+			<SearchBar />
+		</div> //this is JSX
+	);
+};
 
 
 // Take this component's generated HTML and put in on the page (in the Dom)
