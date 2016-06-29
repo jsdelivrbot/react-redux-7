16a47b4e938ec2808cb46c7e75564076d17d1daa
tillman-facecake <touchbase@richtillman.com>
2016-06-29 13:17:17 -0700

    Created video list, list item and video detail

--- ReduxSimpleStarter/src/components/search_bar.js
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -2,11 +2,25 @@
 import React, { Component } from 'react';
 
 // whenever you create a class you must render JSX or it will error
+// NOTE:  'state' is a plain JS object that is used to record and react to user events
+// NOTE:  each class based component, only class based components, has its own 'state' object.  Change of state forces the class component and its children to re-render.
 class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { term: ''};
+	};
+
 	render() {
-		return <input onChange={(event) => console.log(event.target.value)} />;
-	}
-}
+		return (
+			<div>
+				<input 
+					value = {this.state.term}
+					onChange={(event) => this.setState({ term: event.target.value })} />
+			</div>
+		);
+	};
+};
 
 export default SearchBar;
 
--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 // YouTube API key
 const API_KEY = 'AIzaSyCATEQ8h-V4beHQrgUo9Swcs_6q7oNUxT4';
@@ -11,14 +14,39 @@ const API_KEY = 'AIzaSyCATEQ8h-V4beHQrgUo9Swcs_6q7oNUxT4';
 App will never equal 5 or anything other than the assigned function */
 // NOTE: => (fat arrow) is used in place of 'function' and serves an identical purpose.
 // NOTE: When using multi-line JSX the common practice is to wrap in parentheses
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div> //this is JSX
-	);
-};
+// NOTE: This is a functional component.  These are used when taking in info and spitting out JSX
+class App extends Component {
+	constructor(props) {
+		super(props);
 
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			// in ES6 when the name of the key is the same as the value, you can use just 'videos' rather than 'videos: videos'
+			// this.setState({ videos });
+			// resolves to this.setState({videos: videos}) - only works when key and value/property are identically named
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
+		});
+	};
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList
+					onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+					videos={ this.state.videos } />
+			</div> //this is JSX
+		);
+	};
+};
 
 // Take this component's generated HTML and put in on the page (in the Dom)
 // NOTE: in JSX using a self-closing tag when there is no content within is valid. Ex: <App />
