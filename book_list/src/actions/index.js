02057ecbeb0ca4c725bcf4f1f25a8e196a83de61
tillman-facecake <touchbase@richtillman.com>
2016-06-30 17:18:18 -0700

    added action

--- book_list/src/actions/index.js
+++ book_list/src/actions/index.js
@@ -1,3 +1,9 @@
 export function selectBook(book) {
-	console.log('A book has been selected: ', book.title);
+	// selectBook is an ActionCreator and needs to return an action,
+	// an object with a type property.
+	// NOTE: An action always contains a type and sometimes a payload
+	return {
+		type: 'BOOK_SELECTED',
+		payload: book
+	};
 }
