28e5ef095bc25a5a4d5b3fa6053b055321faea2d
Rich Tillman <touchbase@richtillman.com>
2016-07-17 20:22:54 -0700

    Created graphs for pressure and humidity

--- weather/src/containers/weather_list.js
+++ weather/src/containers/weather_list.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart from '../components/chart';
 
 export class WeatherList extends Component {
 	renderWeather(cityData) {
 		const name = cityData.city.name;
 		// Maps over returned array 'list' passes a function named weather with an argument named weather, which drills down to each temp
 		const temps = cityData.list.map(weather => weather.main.temp);
+		const pressures = cityData.list.map(weather => weather.main.pressure);
+		const humidities = cityData.list.map(weather => weather.main.humidity);
 
 		return(
 			<tr key={name}>
 				<td>{name}</td>
 				<td>
-					<Sparklines height={120} width={180} data={temps}>
-						<SparklinesLine color="red" />
-					</Sparklines>
+					<Chart data={temps} color="red" units="K" />
+				</td>
+				<td>
+					<Chart data={pressures} color="blue" units="hPa" />
+				</td>
+				<td>
+					<Chart data={humidities} color="green" units="%" />
 				</td>
 			</tr>
-		)
+		);
 	};
 
 	render() {
@@ -26,9 +32,9 @@ export class WeatherList extends Component {
 				<thead>
 					<tr>
 						<th>City</th>
-						<th>Temperature</th>
-						<th>Pressure</th>
-						<th>Humidity</th>
+						<th>Temperature (K)</th>
+						<th>Pressure (hPa)</th>
+						<th>Humidity (%)</th>
 					</tr>
 				</thead>
 				<tbody>
