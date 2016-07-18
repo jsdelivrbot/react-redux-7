ab72f7379f05cba17826d5defb8d1e9b2d0a7ee3
Rich Tillman <touchbase@richtillman.com>
2016-07-17 19:57:51 -0700

    Included React Sparklines library
    Mapped city temp data to sparklines to create graphs representing
    temperatures in searched cities.

--- weather/src/containers/weather_list.js
+++ weather/src/containers/weather_list.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 export class WeatherList extends Component {
 	renderWeather(cityData) {
 		const name = cityData.city.name;
+		// Maps over returned array 'list' passes a function named weather with an argument named weather, which drills down to each temp
+		const temps = cityData.list.map(weather => weather.main.temp);
 
 		return(
 			<tr key={name}>
 				<td>{name}</td>
+				<td>
+					<Sparklines height={120} width={180} data={temps}>
+						<SparklinesLine color="red" />
+					</Sparklines>
+				</td>
 			</tr>
 		)
 	};
