d0a9e8ff7235d85a8d45e8fbd0e2622d2c5b8952
Rich Tillman <touchbase@richtillman.com>
2016-07-30 14:47:07 -0700

    bug fix

--- blog/src/components/posts_index.js
+++ blog/src/components/posts_index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class PostsIndex extends Component {
+export default class PostsIndex extends Component {
 	componentWillMount() {
 		console.log('this would be a good time to call an action creator to fetch posts');
 	};
