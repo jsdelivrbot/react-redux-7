b79ddf333e05b1652a511b65526cf5448a71000c
Rich Tillman <touchbase@richtillman.com>
2016-07-31 17:29:02 -0700

    Added link to 'add a post'

--- blog/src/components/posts_index.js
+++ blog/src/components/posts_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsIndex extends Component {
 	// componentWillMount is a lifecycle method.  This will be called the first time the dom is loaded/rendered, but not on subsequent re-renders.
@@ -11,7 +12,14 @@ class PostsIndex extends Component {
 
 	render() {
 		return (
-			<div>List fo blog posts</div>
+			<div>
+			<div className="text-xs-right">
+				<Link to="/posts/new" className="btn btn-primary">
+					Add a Post
+				</Link>
+			</div>
+				List of blog posts
+			</div>
 		);
 	};
 };
